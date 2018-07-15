1a3760cc14314acbd1d261e12d9a5be196a3960c
wtimme <wtimme@users.noreply.github.com>
2018-07-15 15:41:47 +0200

    Move `BoundingBox` to dedicated file

--- OSMSwift/Classes/Client/APIClient.swift
+++ OSMSwift/Classes/Client/APIClient.swift
@@ -15,25 +15,6 @@ enum Endpoint: String {
     case getPermissions = "/api/0.6/permissions"
 }
 
-public struct BoundingBox {
-    let left: Double // The longitude of the left (westernmost) side of the bounding box
-    let bottom: Double // The latitude of the bottom (southernmost) side of the bounding box
-    let right: Double // The longitude of the right (easternmost) side of the bounding box
-    let top: Double // The latitude of the top (northernmost) side of the bounding box
-    
-    public init(left: Double, bottom: Double, right: Double, top: Double) {
-        self.left = left
-        self.bottom = bottom
-        self.right = right
-        self.top = top
-    }
-    
-    /// The String representation of the bounding box when using it as an URL parameter.
-    public var queryString: String {
-        return "\(left),\(bottom),\(right),\(top)"
-    }
-}
-
 public protocol APIClientProtocol {
     
     var isAuthenticated: Bool { get }
